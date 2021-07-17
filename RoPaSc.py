7c1b6b2615248d2f1bf91984d1c8f41a79c60598
Inno Mvula <65461268+innomvula@users.noreply.github.com>
2021-07-17 17:27:26 +0000

    Updated

--- RoPaSc.py
+++ RoPaSc.py
@@ -1,38 +1,48 @@
 from random import randint
 
-rps = ['rock', 'paper', 'scissors']
-rounds = 5
-user1score = 0
-cp1score = 0
-for i in range(rounds):
-    j = randint(0,2)
-    cp1 = rps[j]
- #   print(cp1)
-    user1 = input("action: " )
-    if user1 == cp1:
-        print(str(user1score) + '-' + str(cp1score))
-    else:
-        if user1 == 'rock' and cp1 == 'paper':
-            cp1score += 1
-            print(str(user1score) + '-' + str(cp1score))
-        elif user1 == 'rock' and cp1 == 'scissors':
-            user1score += 1
-            print(str(user1score) + '-' + str(cp1score))
-        elif user1 == 'paper' and cp1 == 'rock':
-            user1score += 1
-            print(str(user1score) + '-' + str(cp1score))
-        elif user1 == 'paper' and cp1 == 'scissors':
-            cp1score += 1
-            print(str(user1score) + '-' + str(cp1score))
-        elif user1 == 'scissors' and cp1 == 'rock':
-            cp1score += 1
-            print(str(user1score) + '-' + str(cp1score))
+playagain = "y"
+while playagain == "y":
+    rps = ['rock', 'paper', 'scissors']
+    bestOf = int(input("best of: "))
+    winscore = (bestOf//2) + 1
+    user1score = 0
+    cp1score = 0
+    havewinscore = False
+    while havewinscore == False:
+        j = randint(0,2)
+        cp1 = rps[j]
+    #    print(cp1)
+        user1 = input("rock/paper/scissors: " )
+        if user1 not in rps:
+            print("error: incorrect input")
+            print("score: " + str(user1score) + '-' + str(cp1score))
         else:
-            user1score +=1
-            print(str(user1score) + '-' + str(cp1score))
-if user1score > cp1score:
-    print("User1 is the winner!!")
-elif user1score < cp1score:
-    print("CP1 is the winner!!")
-else:
-    print("Draw")
+            pass
+        if user1 == cp1:
+            print("score: " + str(user1score) + '-' + str(cp1score))
+        else:
+            if user1 == 'rock' and cp1 == 'paper':
+                cp1score += 1
+                print("score: " + str(user1score) + '-' + str(cp1score))
+            elif user1 == 'rock' and cp1 == 'scissors':
+                user1score += 1
+                print("score: " + str(user1score) + '-' + str(cp1score))
+            elif user1 == 'paper' and cp1 == 'rock':
+                user1score += 1
+                print("score: " + str(user1score) + '-' + str(cp1score))
+            elif user1 == 'paper' and cp1 == 'scissors':
+                cp1score += 1
+                print("score: " + str(user1score) + '-' + str(cp1score))
+            elif user1 == 'scissors' and cp1 == 'rock':
+                cp1score += 1
+                print("score: " + str(user1score) + '-' + str(cp1score))
+            elif user1 == 'scissors' and cp1 == 'paper':
+                user1score +=1
+                print("score: " + str(user1score) + '-' + str(cp1score))
+        if user1score == winscore or cp1score == winscore:
+            havewinscore = True
+    if user1score > cp1score:
+        print("User1 is the winner!!")
+    else:
+        print("CP1 is the winner!!")
+    playagain = input("Play again (y/n)?: ")
